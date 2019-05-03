2490c5690b1ec0bd45e6ab7f9c6fda8a3e119d9d
Unknown <mitja.immonen@hotmail.com>
2019-05-03 23:21:15 +0900

    camera lock-on, kinda shit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+	private const float DISTANCE_MARGIN = 1.0f;
+
+	public float distFromPlayer = 4f;
+	public float lockOnXAngle = 30f;
+	public float lockOnYAngle = 30f;
+	public float minDistFromPlayer = 4f;
+	public float maxDistFromPlayer = 6f;
+	public float fovDefault = 70f;
+	public float fovTargeting = 60f;
+	public float lockOnLerpSpeed = 5f;
+	public float lockOffLerpSpeed = 15f;
+
+
+	private float aspectRatio;
+	private float tanFov;
+
+	private Camera cam;
+	private PlayerController player;
+
+
+	void Awake()
+	{
+		var go = GameObject.FindGameObjectWithTag("Player");
+		if (go)
+			player = go.GetComponent<PlayerController>();
+
+		cam = GetComponentInChildren<Camera>();
+		aspectRatio = Screen.width / Screen.height;
+		tanFov = Mathf.Tan(Mathf.Deg2Rad * cam.fieldOfView / 2.0f);
+	}
+
+	void LateUpdate()
+	{
+		if (!player)
+			return;
+
+		SetPosition();
+		SetRotation();
+
+
+	}
+
+	void SetPosition()
+	{
+		Vector3 newPos = Vector3.zero;
+
+		if (player.Target)
+		{
+			Vector3 vectorBetween = player.Target.position - player.GetPos;
+
+			// Calculate the new distance.
+			float distBetween = vectorBetween.magnitude;
+			float cameraDistance = (distBetween / 2.0f / aspectRatio) / tanFov;
+
+			var flatDir = -vectorBetween.normalized;
+			flatDir.y = 0;
+
+			var dir = Quaternion.AngleAxis(lockOnXAngle, Vector3.up) * Quaternion.AngleAxis(lockOnYAngle, transform.right) * flatDir;
+
+			var dist = dir * (cameraDistance + DISTANCE_MARGIN);
+			if (dist.magnitude > maxDistFromPlayer)
+				dist = dist.normalized * maxDistFromPlayer;
+			if (dist.magnitude < minDistFromPlayer)
+				dist = dist.normalized * minDistFromPlayer;
+
+			newPos = player.GetPos + dist;
+			transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * lockOnLerpSpeed);
+
+		}
+		else
+		{
+			Vector3 dirFromPlayer = (transform.position - player.GetPos).normalized;
+			newPos = player.GetPos + dirFromPlayer * distFromPlayer;
+			transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * lockOffLerpSpeed);
+		}
+
+
+	}
+
+	void SetRotation()
+	{
+		if (player.Target)
+		{
+			Vector3 middlepoint = (player.Target.position + player.GetPos) / 2f;
+			transform.rotation = Quaternion.LookRotation((middlepoint - transform.position).normalized);
+		}
+		else
+		{
+			transform.LookAt(player.transform);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Input;
+
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float maxDistToTarget = 10f;
+	[SerializeField] private Transform _target; //Temp for testing
+
+	private Vector2 moveDirection;
+	private Vector2 velocityModifier;
+
+
+	private bool _isRunning;
+	private bool _isGrounded;
+	private bool _isBlocking;
+	private bool _isStunned;
+	private bool _isAttacking;
+
+	public Transform Target
+	{
+		get { return _target; }
+		set { _target = value; }
+	}
+	
+
+	public Vector3 GetPos
+	{
+		get{ return transform.position; }
+	}
+
+    private Inputs inputs;
+
+    void OnEnable()
+    {
+        ControlsSubscribe();
+    }
+    void OnDisable()
+    {
+        ControlsUnsubscribe();
+    }
+
+
+	void Update()
+	{
+		if (Target && (Target.position - transform.position).magnitude > maxDistToTarget)
+		{
+			Target = null;
+		}
+	}
+
+
+    void ControlsSubscribe()
+    {
+        if (inputs == null)
+            inputs = new Inputs();
+
+        inputs.Player.Move.performed += Move;
+		inputs.Player.Move.Enable();
+    }
+    void ControlsUnsubscribe()
+    {
+        inputs.Player.Move.performed -= Move;
+		inputs.Player.Move.Disable();
+	}
+
+
+	#region HandleControls
+
+	void Move(InputAction.CallbackContext context)
+    {
+        Debug.Log("Move vector: "+context.ReadValue<Vector2>());
+    }
+	void Jump(InputAction.CallbackContext context)
+	{
+	
+	}
+	void TargetLock(InputAction.CallbackContext context)
+	{
+
+	}
+	void SwitchTarget(InputAction.CallbackContext context)
+	{
+
+	}
+	void LookAround(InputAction.CallbackContext context)
+	{
+
+	}
+
+
+	void Dodge(InputAction.CallbackContext context)
+	{
+
+	}
+	void Block(InputAction.CallbackContext context)
+	{
+
+	}
+	void Attack(InputAction.CallbackContext context)
+	{
+
+	}
+	void ChargeAttack(InputAction.CallbackContext context)
+	{
+
+	}
+
+
+	#endregion
+
+}
