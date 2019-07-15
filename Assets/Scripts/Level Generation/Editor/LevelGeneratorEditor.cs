1d94ed9611b2e03ed6c96ca58bbf4c1011708b4e
jkgman <jgranger2009@gmail.com>
2019-07-15 10:18:06 +0300

    Made editor code safe

---
+++ Assets/Scripts/Level Generation/Editor/LevelGeneratorEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(LevelGenerator))]
+public class LevelGeneratorEditor : Editor
+{
+    LevelGenerator gen;
+    Editor levelSettings;
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Generate"))
+        {
+            gen.Generate(false);
+        }
+        CreateCachedEditor(gen.settings, null, ref levelSettings);
+        levelSettings.OnInspectorGUI();
+    }
+
+    private void OnEnable()
+    {
+        gen = (LevelGenerator)target;
+    }
+}
+#endif
