5e47faec4fd4b48fd5da9646b81a4414fe8493b2
jkgman <jgranger2009@gmail.com>
2020-03-31 00:35:33 +0300

    Final Clean-up

--- Assets/Scripts/Generation/HL Gen/HLMapSO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "HL Map", menuName = "Generation/HL Map", order = 1)]
-public class HLMapSO : ScriptableObject
-{
-    public HLMap hlmap;
-}
