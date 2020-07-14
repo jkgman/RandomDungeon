768610458459fa59a7c345b22ad8dbac6a98f51f
justin <jgranger2009@gmail.com>
2020-07-14 18:00:30 +0300

    More progress

---
+++ Assets/Scripts/Character/AgentBody.cs
@@ -0,0 +1,172 @@
+﻿using Systems.Targeting;
+using UnityEngine;
+namespace RandomDungeon.Agent
+{
+    public enum AgentState { Idle, Movement, Action }
+    public enum Action { None, Attack, Roll }
+
+    [RequireComponent(typeof(CharacterController))]
+    public class AgentBody : MonoBehaviour
+    {
+        [Header("Scene References")]
+        [SerializeField]
+        protected Animator animator;
+        [Header("Asset References")]
+        [SerializeField]
+        protected TargetSystem targetSystem;
+        [Header("Movement Parameters")]
+        [SerializeField]
+        protected float walkSpeed;
+        [SerializeField]
+        protected float runSpeed;
+        [SerializeField]
+        protected float rotateSpeed = 650;
+
+        protected CharacterController control;
+
+        protected float GetSpeed { get => running ? runSpeed : walkSpeed; }
+
+        //State Info
+        protected AgentState state = AgentState.Idle;
+        protected Target focusTarget;
+        protected bool lockState = false;
+        protected bool running = false;
+        protected Action nextAction = Action.None;
+
+        //Inputs
+        protected Vector3 currentMoveInput;
+        protected Vector3 totalMovementVector;
+
+
+        private void Start()
+        {
+            control = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            //if idle state or walking
+            switch(state)
+            {
+                case AgentState.Idle:
+                    if(nextAction != Action.None)
+                        StartActionState(nextAction);
+                    else if(currentMoveInput.magnitude > 0)
+                        StartMoveState();
+                    break;
+                case AgentState.Movement:
+                    if(nextAction != Action.None)
+                        StartActionState(nextAction);
+                    else if(currentMoveInput.magnitude > 0)
+                        Move();
+                    else
+                        StartIdleState();
+                    break;
+                case AgentState.Action:
+                    if(nextAction != Action.None)
+                        StartActionState(nextAction);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void StartMoveState()
+        {
+            state = AgentState.Movement;
+            if(running)
+                animator.SetTrigger("Run");
+            else
+                animator.SetTrigger("Walk");
+            Move();
+        }
+
+        public void StartIdleState()
+        {
+            state = AgentState.Idle;
+            animator.SetTrigger("Idle");
+        }
+        public void StartActionState(Action action)
+        {
+            if(lockState)
+            {
+                nextAction = action;
+                return;
+            }
+            state = AgentState.Action;
+            //do action
+            switch(action)
+            {
+                case Action.None:
+                    return;
+                case Action.Attack:
+                    break;
+                case Action.Roll:
+                    break;
+                default:
+                    break;
+            }
+
+            //on action finish set to idle state
+            //do nextAction if present
+        }
+
+        public void Move()
+        {
+            Vector3 moveVector = (totalMovementVector.magnitude > 0) ? totalMovementVector : currentMoveInput;
+            moveVector = (moveVector.magnitude <= moveVector.normalized.magnitude) ? moveVector : moveVector.normalized;
+            moveVector *= Time.deltaTime;
+
+            if(focusTarget)
+                LockedOnMovement(moveVector);
+            else
+                FreeMove(moveVector);
+
+            totalMovementVector = Vector2.zero;
+        }
+
+        private void FreeMove(Vector3 moveVector)
+        {
+            moveVector *= GetSpeed;
+            animator.SetFloat("WalkBlend", 0);
+
+            float angle = Vector3.Angle(transform.forward, moveVector);
+            Vector3 Move = transform.forward * moveVector.magnitude * ((360 - angle) / 360);
+            control.Move(Move);
+
+            if(angle > Time.deltaTime * rotateSpeed)
+            {
+                transform.Rotate(Vector3.up, Time.deltaTime * rotateSpeed * Mathf.Sign(Vector3.SignedAngle(transform.forward, moveVector, Vector3.up)));
+            }
+        }
+
+        private void LockedOnMovement(Vector3 moveVector)
+        {
+            moveVector *= GetSpeed;
+            Vector2 dir = new Vector2(moveVector.x, moveVector.z);
+            float dirAngle = Vector2Extensions.GetDegree(dir);
+            float targetAngle = Vector2Extensions.GetDegree(focusTarget.transform.position - transform.position);
+            float result = (dirAngle - targetAngle) - Mathf.CeilToInt((dirAngle - targetAngle) / 360f) * 360f;
+            if(result < 0)
+            {
+                result += 360f;
+            }
+            animator.SetFloat("WalkBlend", result);
+            control.Move(moveVector);
+            transform.LookAt(new Vector3(focusTarget.transform.position.x, transform.position.y, focusTarget.transform.position.z), Vector3.up);
+        }
+
+
+        public void FocusTarget()
+        {
+            focusTarget = targetSystem.FindClosestTarget(transform.position);
+        }
+
+        public void UnfocusTarget()
+        {
+            focusTarget = null;
+        }
+
+
+    }
+}
--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -6,27 +6,13 @@ using Systems.Targeting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace INDEV.Player
+namespace RandomDungeon.Agent
 {
     public enum MoveState { Idle, Walk, Run, Attack, Roll }
 
     [RequireComponent(typeof(CharacterController))]
-    public class PlayerController : MonoBehaviour
+    public class PlayerController : AgentBody
     {
-        [Header("Scene References")]
-        [SerializeField]
-        private Animator animator;
-        [Header("Asset References")]
-        [SerializeField]
-        private TargetSystem targetSystem;
-        [Header("Control Parameters")]
-        [SerializeField]
-        private float walkSpeed;
-        [SerializeField]
-        private float runSpeed;
-        [SerializeField]
-        private float rotateSpeed = 90;
-
         [Header("Roll Parameters")]
         [SerializeField]
         private float rollMagnitude = 1;
@@ -42,23 +28,7 @@ namespace INDEV.Player
             { MoveState.Attack, "Attack"},
             { MoveState.Roll, "Roll"}
         };
-        private CharacterController control;
 
-        private MoveState currentState = MoveState.Idle;
-
-        private bool running = false;
-
-        private bool lockedOn = false;
-        private Target lockOnTarget;
-
-        private Vector3 direction = Vector3.forward;
-        private Vector3 currentMoveInput;
-        private Vector3 totalMovementVector;
-
-        private void Start()
-        {
-            control = GetComponent<CharacterController>();
-        }
 
         #region Input Handling
 
@@ -72,143 +42,75 @@ namespace INDEV.Player
 
         public void OnRun(InputAction.CallbackContext context)
         {
-            if(context.started) { 
-                lockedOn = false;
+            if(context.started)
+            {
+                UnfocusTarget();
                 running = true;
+                if(state == AgentState.Movement)
+                {
+                    animator.SetTrigger("Run");
+                }
             }
-            else if(context.canceled)
+            else if(context.canceled) {
                 running = false;
+                if(state == AgentState.Movement)
+                {
+                    animator.SetTrigger("Walk");
+                }
+            }
         }
 
         public void OnTargetLock(InputAction.CallbackContext context)
         {
-            if(!context.started)
+            if(!context.started || running)
                 return;
 
-            if(!lockedOn)
+            if(focusTarget == null)
             {
-                lockedOn = true;
-                lockOnTarget = targetSystem.FindClosestTarget(transform.position);
+                FocusTarget();
             }
             else
             {
-                lockedOn = false;
+                UnfocusTarget();
             }
         }
 
-        public void OnAttack(InputAction.CallbackContext context) {
-            if(!context.started || currentState == MoveState.Roll || currentState == MoveState.Attack)
+        public void OnAttack(InputAction.CallbackContext context) 
+        {
+            if(!context.started)
                 return;
-            Attack();
+            nextAction = Action.Attack;
         }
 
         public void OnRoll(InputAction.CallbackContext context)
         {
-            if(!context.started || currentState == MoveState.Roll || currentState == MoveState.Attack)
+            if(!context.started)
                 return;
-            Roll();
+            nextAction = Action.Roll;
         }
 
         #endregion Input Handling
 
-        void Update()
-        {
-            if(currentState == MoveState.Roll || currentState == MoveState.Attack)
-            {
-                return;
-            }
-            if(!EvaluateMovement())
-            {
-                ChangeState(MoveState.Idle);
-            }
-        }
 
-        private bool EvaluateMovement()
-        {
-            bool moved = false;
-            Vector3 moveVector = (totalMovementVector.magnitude > 0) ? totalMovementVector : currentMoveInput;
-            moveVector = (moveVector.magnitude <= moveVector.normalized.magnitude) ? moveVector : moveVector.normalized;
-            moveVector *= Time.deltaTime;
-
-            if(moveVector.magnitude > 0)
-            {
-                if(lockedOn)
-                    LockedOnMovement(moveVector);
-                else
-                    FreeMove(moveVector);
-                moved = true;
-            }
-            totalMovementVector = Vector2.zero;
-            direction = transform.forward;
-            return moved;
-        }
-
-        private void FreeMove(Vector3 moveVector) 
-        {
-            if(running)
-            {
-                moveVector *= runSpeed;
-                ChangeState(MoveState.Run);
-            }
-            else
-            {
-                moveVector *= walkSpeed;
-                ChangeState(MoveState.Walk);
-                animator.SetFloat("WalkBlend", 0);
-                //    Vector2 dir = new Vector2(moveVec.x, moveVec.z);
-                //    float angle = Vector2Extensions.GetDegree(dir);
-                //    animator.SetFloat("WalkBlend", angle);
-            }
-
-            float angle = Vector3.Angle(direction, moveVector);
-            Vector3 Move = direction * moveVector.magnitude * ((360 - angle) / 360);
-            control.Move(Move);
-
-            if(angle > Time.deltaTime * rotateSpeed)
-            {
-                transform.Rotate(Vector3.up, Time.deltaTime * rotateSpeed * Mathf.Sign(Vector3.SignedAngle(direction, moveVector, Vector3.up)));
-            }
-        }
-
-        private void LockedOnMovement(Vector3 moveVector) {
-            moveVector *= walkSpeed;
-            ChangeState(MoveState.Walk);
-            Vector2 dir = new Vector2(moveVector.x, moveVector.z);
-            float dirAngle = Vector2Extensions.GetDegree(dir);
-            float targetAngle = Vector2Extensions.GetDegree(lockOnTarget.transform.position - transform.position);
-            float result = (dirAngle - targetAngle) - Mathf.CeilToInt((dirAngle - targetAngle) / 360f) * 360f;
-            if(result < 0)
-            {
-                result += 360f;
-            }
-            animator.SetFloat("WalkBlend", result);
-            control.Move(moveVector);
-            transform.LookAt(new Vector3(lockOnTarget.transform.position.x,transform.position.y, lockOnTarget.transform.position.z), Vector3.up);
-        }
-        
         #region Priority States
 
         //priority states cancel whatever your currently doing to execute them if current state is cancelable, otherwise queues up for next action
         private async Task Attack()
         {
-            ChangeState(MoveState.Attack);
             await Awaiters.Seconds(.867f);
             await Awaiters.NextFrame;
             totalMovementVector = Vector2.zero;
-            currentState = MoveState.Idle;
         }
 
         private async Task Roll()
         {
-            ChangeState(MoveState.Roll);
             Vector3 dir;
             if(currentMoveInput.magnitude > 0)
                 dir = currentMoveInput.normalized;
             else
-                dir = direction;
+                dir = transform.forward;
             await AsyncAnimation.GlobalTranslate(transform, transform.position + dir * rollMagnitude, rollCurve, 2.4f);
             totalMovementVector = Vector2.zero;
-            currentState = MoveState.Idle;
         }
         #endregion Priority States
 
@@ -221,15 +123,6 @@ namespace INDEV.Player
             }
         }
 
-        private async Task Move(Vector3 destination, AnimationCurve curve, float duration) {
 
-        }
-
-        private void ChangeState(MoveState state) {
-            if(state != currentState) {
-                animator.SetTrigger(stateDictionary[state]);
-                currentState = state;
-            }
-        }
     }
 }
